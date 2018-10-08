a45cd7edbdd0aaf55c85b2e3c9a33bc312a4991d
杜印 <m18612326243@163.com>
2018-10-08 02:42:03 -0700

    修改访问域名文件

--- enterprise_atlas/js/config.js
+++ enterprise_atlas/js/config.js
@@ -1,8 +1,13 @@
+//获取本地域名
+var domain = window.location.host;
+var protocol = document.location.protocol;
+//获取当前域名
+var dataUrl = protocol + "//" + domain + "/familyTree";
 //开发环境
-var dataUrlDev = "http://10.76.3.74:8081";
+// var dataUrlDev = "http://10.76.3.74:8081";
 
 // 测试环境
-var dataUrl = "http://10.76.3.74:8081";
+// var dataUrl = "http://10.76.3.74:8081";
 
 // 生产环境
-var dataUrlPro = "https://appdata-intra.c.citic";
+// var dataUrlPro = "https://appdata-intra.c.citic";
